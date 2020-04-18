689ea2ec2cc8bdd75ced23a9c9b5bfa5472b7e4b
Angelica Landazabal <angelica.informatik@gmail.com>
2020-04-18 13:16:42 -0500

    Presentation FLISOL

---
+++ twitterCredentials.py
@@ -0,0 +1,6 @@
+# TWITTER CREDENTIALS
+
+api_key = "CEK2hKZae80HJra0qZO0B37UB"
+api_secret = "WpA7evqVXlZ5aL2Wvkz4Olr8QqwN5VNWbRtYzrMtctOnnqKbUJ"
+access_token = "199935768-sKmUj5jWjSfZchAQLetruRlR4QNNSFhEXPT2R9bo"
+access_secret = "CnpzwMhIzDyUS8VSuY7QfguRtnggh4sFq97PUMQKdBBQt"
